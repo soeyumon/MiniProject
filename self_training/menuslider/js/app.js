e14446aeb97e1952f650cfa1696147cd478b02a9
Soe Yu Mon <soeyumonkse1995@gmail.com>
2021-05-17 11:06:56 +0630

    17/May/2021

--- self_training/menuslider/js/app.js
+++ self_training/menuslider/js/app.js
@@ -4,28 +4,24 @@ const open = document.getElementById("open");
 const close = document.getElementById("close");
 const modal = document.getElementById("modal");
 
-// Add Event Listener
+// Event Listener
 toggle.addEventListener("click",()=>{
     document.body.classList.toggle("show-nav");
 });
-// Show Modal
+
 open.addEventListener("click",()=>{
+    // console.log("abcd");
     modal.classList.add("show-modal");
 });
 
-// Hide Modal
 close.addEventListener("click",()=>{
     modal.classList.remove("show-modal");
-});
-
-// Hide Modal by clicking opacity
-// METHOE 1
+})
+// method1
 // window.addEventListener("click",function(e){
-//     // console.log(e.target);
 //     if(e.target === modal){
 //         modal.classList.remove("show-modal");
 //     }
 // });
-
-// METHOE2
-window.addEventListener('click',(e)=>e.target === modal?modal.classList.remove("show-modal"):false);
+// Method2
+window.addEventListener('click',(e)=>e.target === modal ? modal.classList.remove("show-modal"):false);
